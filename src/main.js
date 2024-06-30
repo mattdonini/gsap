d252aec9aec14db12a0879bc80f6fc159a30d77a
Matteo Donini <167872740+mattdonini@users.noreply.github.com>
2024-06-30 20:11:36 +0200

    main.js

--- src/main.js
+++ src/main.js
@@ -2,26 +2,35 @@ import './styles/style.css';
 import { gsap } from "gsap";
 
 document.addEventListener('DOMContentLoaded', () => {
+  console.log('DOM fully loaded and parsed');
   const titles = document.querySelectorAll('.threads_title-item');
   const triggers = document.querySelectorAll('.threads_trigger-item');
+
+  console.log('titles:', titles);
+  console.log('triggers:', triggers);
 
   // Hide all title_wrap elements initially and set them above the viewport
   titles.forEach(item => {
     const titleWrap = item.querySelector('.title_wrap');
-    gsap.set(titleWrap, { opacity: 0, y: '-100%', visibility: 'hidden' });
+    const title = titleWrap.querySelector('.title');
+    const threadNumber = titleWrap.querySelector('.thread-number');
+
+    gsap.set(titleWrap, { opacity: 0, visibility: 'hidden' });
+    gsap.set(title, { y: '-100%' });
+    gsap.set(threadNumber, { y: '-50%' });
   });
 
   const slideIn = (target) => {
     gsap.killTweensOf(target);
     gsap.fromTo(target, 
-      { opacity: 0, y: '-10%', visibility: 'visible' }, 
-      { opacity: 1, y: '0%', duration: 0.1, ease: 'power2.out' });
+      { opacity: 0, y: '-20%', visibility: 'visible' }, 
+      { opacity: 1, y: '0%', duration: 0.2, ease: 'power2.out' });
   };
 
   const slideOut = (target) => {
     gsap.killTweensOf(target);
-    return gsap.to(target,
-      { opacity: 0, y: '10%', duration: 0.1, ease: 'power2.in', onComplete: () => {
+    return gsap.to(target, 
+      { opacity: 0, y: '20%', duration: 0.2, ease: 'power2.in', onComplete: () => {
         target.style.visibility = 'hidden';
       }});
   };
@@ -33,6 +42,8 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log("Default Target:", defaultTarget);  // Debug output
     if (defaultTarget) {
       gsap.set(defaultTarget, { opacity: 1, y: '0%', visibility: 'visible' });
+      gsap.set(defaultTarget.querySelector('.title'), { y: '0%' });
+      gsap.set(defaultTarget.querySelector('.thread-number'), { y: '0%' });
     } else {
       console.error(`No matching target found with data-threads-id="${firstTriggerId}"`);
     }
@@ -44,6 +55,7 @@ document.addEventListener('DOMContentLoaded', () => {
       const target = document.querySelector(`.threads_title-item[data-threads-id="${id}"] .title_wrap`);
 
       if (target) {
+        console.log(`Animating target with id: ${id}`);
         // Create a timeline to sequence the slide out and slide in animations
         const tl = gsap.timeline();
 
